6d79146f74a5f14f909bfcd2838aa6244ae7386f
Justin Marrington <justin@marrington.net>
2016-03-25 18:30:55 +1000

    Implement Counter display component.

--- src/components/App/__tests__/App-tests.js
+++ src/components/App/__tests__/App-tests.js
@@ -3,7 +3,10 @@
 import React from "react";
 import {shallow} from "enzyme";
 import {expect} from "chai";
+import {Provider} from "react-redux";
+
 import App from "../";
+import store from "../../../store";
 
 describe("App", function() {
   let component;
@@ -16,5 +19,13 @@ describe("App", function() {
     const header = component.find("h1");
 
     expect(header.text()).to.contain("Counter App");
+  });
+
+  it("should wrap the top-level components in a Redux Provider", () => {
+    expect(component.is(Provider)).to.equal(true);
+  });
+
+  it("should set the application redux store to the Provider's prop", () => {
+    expect(component.props().store).to.equal(store);
   });
 });
---
+++ src/components/Counter/display/Counter.js
@@ -0,0 +1,40 @@
+"use strict";
+
+import React from "react";
+
+const Counter = ({
+  name,
+  count,
+  onIncrementClicked,
+  onDecrementClicked
+}) => (
+  <div className="counter">
+    <h2 className="counter-name">{name}</h2>
+
+    <div className="counter-count">
+      {count}
+    </div>
+
+    <div className="counter-actions">
+      <button className="button counter-increment"
+        onClick={onIncrementClicked}
+      >
+        +
+      </button>
+      <button className="button counter-decrement"
+        onClick={onDecrementClicked}
+      >
+        -
+      </button>
+    </div>
+  </div>
+);
+Counter.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  count: React.PropTypes.number.isRequired,
+  onIncrementClicked: React.PropTypes.function,
+  onDecrementClicked: React.PropTypes.function
+};
+
+export default Counter;
+
